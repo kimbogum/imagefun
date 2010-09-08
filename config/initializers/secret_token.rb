58b6a38bf053fd880b8b6e3e5794d09808f955b7
Justin Wood <kimbogum@gmail.com>
2010-09-08 19:02:38 +1200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Imagefun::Application.config.secret_token = '3af0aab74214509d20f74900ab66a6b885eb0a7d465107f58f53045348c1bdef3172474239e5212d31050d0c701ae720d40623ab95828a8462cde7727d9ad2d9'
