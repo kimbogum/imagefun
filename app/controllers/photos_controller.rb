7799da0484dd814a7dd3bc810822bf186dde4f4d
Marcus Wyatt <marcus.wyatt@visfleet.com>
2010-09-08 19:48:56 +1200

    New ActionController Syntax

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,72 +1,64 @@
 class PhotosController < ApplicationController
+  
+  respond_to :html, :xml, :json
+  
   # GET /photos
   # GET /photos.xml
   def index
-    @photos = Photo.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @photos }
-    end
+    respond_with(@photos = Photo.all)
   end
 
   # GET /photos/1
   # GET /photos/1.xml
   def show
-    @photo = Photo.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @photo }
-    end
+    respond_with(@photo = Photo.find(params[:id]))
   end
 
   # GET /photos/new
   # GET /photos/new.xml
   def new
     @photo = Photo.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @photo }
-    end
+    respond_with(@photo)
   end
 
   # GET /photos/1/edit
   def edit
     @photo = Photo.find(params[:id])
+    respond_with(@photo)
   end
 
   # POST /photos
   # POST /photos.xml
   def create
-    @photo = Photo.new(params[:photo])
-
-    respond_to do |format|
-      if @photo.save
-        format.html { redirect_to(@photo, :notice => 'Photo was successfully created.') }
-        format.xml  { render :xml => @photo, :status => :created, :location => @photo }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @photo.errors, :status => :unprocessable_entity }
-      end
+    @photo = Photo.create(params[:photo])
+    respond_with(@photo, :location => @photo) do |format|
+      format.xml { render :template => "photos/show", :locals => {:photo  => @photo } }
     end
+    # respond_to do |format|
+    #   if @photo.save
+    #     format.html { redirect_to(@photo, :notice => 'Photo was successfully created.') }
+    #     format.xml  { render :xml => @photo, :status => :created, :location => @photo }
+    #   else
+    #     format.html { render :action => "new" }
+    #     format.xml  { render :xml => @photo.errors, :status => :unprocessable_entity }
+    #   end
+    # end
   end
 
   # PUT /photos/1
   # PUT /photos/1.xml
   def update
     @photo = Photo.find(params[:id])
-
-    respond_to do |format|
-      if @photo.update_attributes(params[:photo])
-        format.html { redirect_to(@photo, :notice => 'Photo was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @photo.errors, :status => :unprocessable_entity }
-      end
-    end
+    respond_with(@photo.update_attributes(params[:photo]))
+    # respond_to do |format|
+    #   if 
+    #     format.html { redirect_to(@photo, :notice => 'Photo was successfully updated.') }
+    #     format.xml  { head :ok }
+    #   else
+    #     format.html { render :action => "edit" }
+    #     format.xml  { render :xml => @photo.errors, :status => :unprocessable_entity }
+    #   end
+    # end
   end
 
   # DELETE /photos/1
@@ -74,10 +66,10 @@ class PhotosController < ApplicationController
   def destroy
     @photo = Photo.find(params[:id])
     @photo.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(photos_url) }
-      format.xml  { head :ok }
-    end
+    respond_with(@photo)
+    # respond_to do |format|
+    #   format.html { redirect_to(photos_url) }
+    #   format.xml  { head :ok }
+    # end
   end
 end
