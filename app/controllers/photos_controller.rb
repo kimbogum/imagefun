e3628a8174eaffd201bc2fe6a1035091f08167a0
Marcus Wyatt <marcus.wyatt@visfleet.com>
2010-09-08 19:58:12 +1200

    Fixed the bugs with the New ActionController Syntax

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -17,14 +17,12 @@ class PhotosController < ApplicationController
   # GET /photos/new
   # GET /photos/new.xml
   def new
-    @photo = Photo.new
-    respond_with(@photo)
+    respond_with(@photo = Photo.new)
   end
 
   # GET /photos/1/edit
   def edit
-    @photo = Photo.find(params[:id])
-    respond_with(@photo)
+    respond_with(@photo = Photo.find(params[:id]))
   end
 
   # POST /photos
@@ -32,44 +30,22 @@ class PhotosController < ApplicationController
   def create
     @photo = Photo.create(params[:photo])
     respond_with(@photo, :location => @photo) do |format|
+      format.html { redirect_to photos_url }
       format.xml { render :template => "photos/show", :locals => {:photo  => @photo } }
     end
-    # respond_to do |format|
-    #   if @photo.save
-    #     format.html { redirect_to(@photo, :notice => 'Photo was successfully created.') }
-    #     format.xml  { render :xml => @photo, :status => :created, :location => @photo }
-    #   else
-    #     format.html { render :action => "new" }
-    #     format.xml  { render :xml => @photo.errors, :status => :unprocessable_entity }
-    #   end
-    # end
   end
 
   # PUT /photos/1
   # PUT /photos/1.xml
   def update
     @photo = Photo.find(params[:id])
-    respond_with(@photo.update_attributes(params[:photo]))
-    # respond_to do |format|
-    #   if 
-    #     format.html { redirect_to(@photo, :notice => 'Photo was successfully updated.') }
-    #     format.xml  { head :ok }
-    #   else
-    #     format.html { render :action => "edit" }
-    #     format.xml  { render :xml => @photo.errors, :status => :unprocessable_entity }
-    #   end
-    # end
+    @photo.update_attributes(params[:photo])
+    respond_with(@photo)
   end
 
   # DELETE /photos/1
   # DELETE /photos/1.xml
   def destroy
-    @photo = Photo.find(params[:id])
-    @photo.destroy
-    respond_with(@photo)
-    # respond_to do |format|
-    #   format.html { redirect_to(photos_url) }
-    #   format.xml  { head :ok }
-    # end
+    respond_with(Photo.find(params[:id]).destroy)
   end
 end
